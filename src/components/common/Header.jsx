2215b2e9bdff8444a198a0cf6d0e72a31bf4e952
charsskim <mcprince73@naver.com>
2023-12-06 11:42:11 +0900

    서브페이지 클릭할때해당링크로 이동

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,13 +1,33 @@
+import { Link, NavLink } from 'react-router-dom';
+
 export default function Header() {
 	return (
 		<header className='Header'>
-			<h1>Logo</h1>
+			<h1>
+				<Link to='/'>Logo</Link>
+			</h1>
 
 			<ul>
-				<li>Department</li>
-				<li>Youtube</li>
-				<li>Gallery</li>
-				<li>Location</li>
+				<li>
+					<NavLink to='/department' activeClassName={'on'}>
+						Department
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/youtube' activeClassName={'on'}>
+						Youtube
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/gallery' activeClassName={'on'}>
+						Gallery
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/location' activeClassName={'on'}>
+						Location
+					</NavLink>
+				</li>
 			</ul>
 		</header>
 	);
